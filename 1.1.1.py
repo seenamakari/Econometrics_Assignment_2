1d1277a3d2cc325b34bd463a9c02d708edf517d0
seenamakari <seenamakari@gmail.com>
2023-11-09 14:35:00 -0500

    Update 1.1.1.py

--- 1.1.1.py
+++ 1.1.1.py
@@ -1,21 +1,28 @@
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
 
-# Changed dataset into a DataFrame
+# Load the dataset into a DataFrame
 df = pd.read_csv('Stocks.csv')
 
-# Calculate simple returns from one row to the next
+# Calculating simple returns from one row to the next
 df['Simple_Returns'] = df['Close'].pct_change()
-
-# Drop rows with NaN values (which will be the first row after calculating returns)
 df = df.dropna(subset=['Simple_Returns'])
 
-simple_return = (df['Close'] / df['Close'].shift(1)) - 1
-simple_return = simple_return.dropna()
+df['Log_Returns'] = np.log(df['Close']) - np.log(df['Close'].shift(1))
+df = df.dropna(subset=['Log_Returns'])
 
-log_return = np.log(df['Close']) - np.log(df['Close'].shift(1))
-log_return = log_return.dropna()
+simple_returns_column = df['Simple_Returns']
 
-print(simple_return)
-print (log_return)
+# Fit an AutoReg(10) model
+lags = 10  # Number of lags (AR terms)
+model = sm.tsa.AutoReg(simple_returns_column, lags=lags)
+results = model.fit()
+
+print(results.summary())
+
+#df_simple_returns = df[['Date', 'Simple_Returns']]
+#df_simple_returns = df[['Date', 'Simple_Returns']]
+#print(df_simple_returns)
+#pd.plotting.lag_plot(df_simple_returns, lag=1)
 
