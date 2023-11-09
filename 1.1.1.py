3cd42b1b72564bc669781259d9006e68466dd921
seenamakari <seenamakari@gmail.com>
2023-11-08 19:45:18 -0500

    Update 1.1.1.py

--- 1.1.1.py
+++ 1.1.1.py
@@ -4,3 +4,10 @@ import numpy as np
 # Changed dataset into a DataFrame
 df = pd.read_csv('Stocks.csv')
 
+# Calculate simple returns from one row to the next
+df['Simple_Returns'] = df['Close'].pct_change()
+
+# Drop rows with NaN values (which will be the first row after calculating returns)
+df = df.dropna(subset=['Simple_Returns'])
+
+df['simple_return'] = (df['Close'] / df['Close'].shift(1)) - 1
