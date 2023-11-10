d7c33727ac24952694d885fd9eadb57599f2b5fb
seenamakari <seenamakari@gmail.com>
2023-11-10 02:09:23 -0500

    Update 2.1.1.py

--- 2.1.1.py
+++ 2.1.1.py
@@ -9,7 +9,29 @@ df = pd.read_csv('Canada.csv')
 
 df['date'] = pd.to_datetime(df['date'])
 df.set_index('date', inplace=True)
+# Plotting M2++ (gross)
+df['M2++ (gross)'].plot()
+plt.title('M2++ (gross)')
+plt.xlabel('Date')
+plt.ylabel('Value')
+plt.show()
 
+# Plotting CPI
+df['CPI)'].plot()
+plt.title('CPI')
+plt.xlabel('Date')
+plt.ylabel('Value')
+plt.show()
+
+# Plotting Bank Rate
+df['Bank Rate'].plot()
+plt.title('Bank Rate')
+plt.xlabel('Date')
+plt.ylabel('Value')
+plt.show()
+
+model = VAR(df)
+results = model.fit(1)  # Fit a VAR(1) model
 print(results.summary())
 
 
